8780047f82453df52979d2840dd83e544a5e9ff3
Aashutosh Shrestha <aashutoshrestha@outlook.com>
2024-05-11 21:13:29 +0545

    Initialized Project

---
+++ GoogleGeminiSampleApp/GoogleGeminiSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleGeminiSampleAppApp.swift
+//  GoogleGeminiSampleApp
+//
+//  Created by Aashutosh Shrestha on 5/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GoogleGeminiSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
