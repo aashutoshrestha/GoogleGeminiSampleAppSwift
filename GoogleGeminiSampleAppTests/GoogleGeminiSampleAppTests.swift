e0fcea27818ec26e2228dc1ca0472d6797dd8589
Aashutosh Shrestha <aashutoshrestha@outlook.com>
2024-05-13 15:39:09 +0545

    Added test cases

--- GoogleGeminiSampleAppTests/GoogleGeminiSampleAppTests.swift
+++ GoogleGeminiSampleAppTests/GoogleGeminiSampleAppTests.swift
@@ -9,28 +9,44 @@ import XCTest
 @testable import GoogleGeminiSampleApp
 
 final class GoogleGeminiSampleAppTests: XCTestCase {
-
+    
+    
+    var bookViewModel: BookViewModel!
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        bookViewModel = BookViewModel()
     }
-
+    
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        bookViewModel = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testBookViewModel() throws {
+        // Given
+        let searchTerm = "Harry Potter"
+        
+        // When
+        bookViewModel.fetchBooks(searchTerm: searchTerm)
+        
+        // Then
+        XCTAssertTrue(bookViewModel.isLoading)
+        
+        // Wait for some time for the request to complete
+        let expectation = XCTestExpectation(description: "Fetch Books")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            XCTAssertNotNil(self.bookViewModel.booksResponse)
+            XCTAssertFalse(self.bookViewModel.isLoading)
+            XCTAssertNil(self.bookViewModel.error)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 10.0)
     }
-
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
 }
