d3c925f97bfd7b0532464f40f7e7e1a302615c4f
Aashutosh Shrestha <aashutoshrestha@outlook.com>
2024-05-14 10:30:14 +0545

    Added Persistent data for API Keys

---
+++ GoogleGeminiSampleApp/Services/SaveData.swift
@@ -0,0 +1,11 @@
+//
+//  UserDefaults.swift
+//  GoogleGeminiSampleApp
+//
+//  Created by Aashutosh Shrestha on 5/13/24.
+//
+
+import Foundation
+class UserDefaults{
+    
+}
---
+++ GoogleGeminiSampleApp/Views/Settings/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  GoogleGeminiSampleApp
+//
+//  Created by Aashutosh Shrestha on 5/13/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
